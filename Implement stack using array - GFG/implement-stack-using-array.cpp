f9bc8a65eb39c807deb04e3a1a783fa9c2f736ac
ankitam777 <122123021+ankitam777@users.noreply.github.com>
2023-04-01 20:18:31 +0530

    Added solution - LeetHub

--- Implement stack using array - GFG/implement-stack-using-array.cpp
+++ Implement stack using array - GFG/implement-stack-using-array.cpp
@@ -63,17 +63,15 @@ public:
 void MyStack :: push(int x)
 {
     // Your Code
-    top++;
-    arr[top] = x;
+    top=top+1;
+    arr[top]=x;
 }
 
 //Function to remove an item from top of the stack.
 int MyStack :: pop()
 {
-    // Your Code   
-   if(top==-1)
-   return -1;
-   int x = arr[top];
-    top--;
-    return x;
+    // Your Code       
+    if(top==-1)
+    return -1;
+    return arr[top--];
 }
