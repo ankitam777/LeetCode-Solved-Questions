df9b2503abe11ec063641afe23e41dcc78df3749
ankitam777 <122123021+ankitam777@users.noreply.github.com>
2023-04-04 16:16:31 +0530

    Added solution - LeetHub

---
+++ Prime Factors - GFG/prime-factors.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+	public:
+	vector<int>AllPrimeFactors(int N) {
+	    // Code here
+	    set<int>st;
+	    vector<int>v;
+	    for(int i=2;i<=N;i++){
+	        while(N%i==0){
+	            if(st.find(i)==st.end()){
+	                st.insert(i);
+	                v.push_back(i);
+	            }
+	            N=N/i;
+	        }
+	    }
+	    return v;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		long long int N;
+		cin >> N;
+		Solution ob;
+		vector<int>ans = ob.AllPrimeFactors(N);
+		for(auto i: ans)
+			cout << i <<" ";
+		cout <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
