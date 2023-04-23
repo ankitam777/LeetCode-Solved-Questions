89c66236d086f0f5fe6d870a2e6cea2a96d2d5cb
ankitam777 <122123021+ankitam777@users.noreply.github.com>
2023-04-23 18:43:23 +0530

    Added solution - LeetHub

---
+++ Key Pair - GFG/key-pair.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int A[], int arr_size, int sum) {
+	    // code here
+	    int l, r;
+ 
+    /* Sort the elements */
+    sort(A, A + arr_size);
+ 
+    /* Now look for the two candidates in
+       the sorted array*/
+    l = 0;
+    r = arr_size - 1;
+    while (l < r) {
+        if (A[l] + A[r] == sum)
+            return 1;
+        else if (A[l] + A[r] < sum)
+            l++;
+        else // A[l] + A[r] > sum
+            r--;
+    }
+    return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
