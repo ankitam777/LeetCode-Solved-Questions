c4008f01fcb630663d2f51465798cec2f0262808
ankitam777 <122123021+ankitam777@users.noreply.github.com>
2023-06-18 01:32:03 +0530

    Added solution - LeetHub

---
+++ Length of the longest substring - GFG/length-of-the-longest-substring.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int longestUniqueSubsttr(string s){
+        //code
+        //unordered_map<char, int> chars;
+        int chars[256]={0};
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s[right];
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s[left];
+                chars[l]--;
+                left++;
+            }
+
+            res = max(res, right - left + 1);
+
+            right++;
+        }
+
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string str;
+		cin>>str;
+		Solution ob;
+		cout<<ob.longestUniqueSubsttr(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
