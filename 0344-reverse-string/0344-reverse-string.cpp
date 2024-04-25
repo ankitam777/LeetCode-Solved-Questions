6ed0161e7867b5a6ff8a35d61f67d114f77404d0
Ankita Mukherjee <122123021+ankitam777@users.noreply.github.com>
2024-04-25 17:55:28 +0530

    Time: 20 ms (39.50%), Space: 27 MB (21.91%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         char temp;
         int n=s.size();
         for(int i=0;i<s.size()/2;i++){
-           temp=s[i];
-           s[i]=s[n-i-1];
-           s[n-i-1]=temp;
+           swap(s[i],s[n-i-1]);
         }
         
     }
